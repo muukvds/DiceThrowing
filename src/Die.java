b693745dfed406b4af175527bcbe20cfca337518
muukvds <muukvds@users.noreply.github.com>
2017-09-21 12:00:18 +0200

    create project, add Die class

---
+++ src/Die.java
@@ -0,0 +1,18 @@
+public class Die {
+    private int numberOfSides;
+
+
+    public void setNumberOfSides(int sides) {
+    }
+
+    public int getNumberOfSides() {
+        return numberOfSides;
+    }
+
+    public int getThrow() {
+        int diesTrows =0;
+
+
+        return diesTrows;
+    }
+}
