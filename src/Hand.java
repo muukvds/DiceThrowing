2b8f9becfea83b94822c27ddd919b10ebc19c66b
muukvds <muukvds@users.noreply.github.com>
2017-09-21 12:45:00 +0200

    add addDie method to Hand

--- src/Hand.java
+++ src/Hand.java
@@ -1,2 +1,13 @@
 public class Hand {
+    private Die[] dies = new Die[6];
+
+
+    public void addDie(Die die) {
+        for (int i = 0; i < dies.length; i++) {
+            if (dies[i] == null) {
+                dies[i] = die;
+            }
+        }
+
+    }
 }
